506469020489b890cebf4e9906d80ad9f9630977
memusic <89031071+memusic@users.noreply.github.com>
2021-08-19 12:00:22 -0400

    Add files via upload
    ..

---
+++ sourcecode/app/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define( "debug", false );
+define( "debug_for_ip", false );
+define( "debug_for_admin", false );
+
+
+define( "session_check_ratio", 25 );
+
+define( "db_host", "localhost" );
+define( "db_user", "root" );
+define( "db_pass", "" );
+define( "db_name", "memusic" );
+define( "db_char", "UTF8" );
+
+define( "firewall", false );
+define( "firewall_anti_session_hijacking", false );
+define( "firewall_anti_session_hijacking_max_sessions_per_ip_per_day", 10 );
+
+?>
